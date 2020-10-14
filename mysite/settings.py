021d6bc53935c9bafa15403477b7cd5541225f6b
tdelga <48927952+tdelga@users.noreply.github.com>
2020-10-13 21:46:59 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-BASE_DIR = os.path.join(BASE_DIR('staticfiles'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
