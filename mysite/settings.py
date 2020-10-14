d73e7db53d64a5726bcd377abd3d1ce0d2f97cb5
tdelga <48927952+tdelga@users.noreply.github.com>
2020-10-13 21:37:38 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.cofig(
+    'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 }
