968a8e97ae72dcfb8277d140503c3ae0312ed880
juliGer <juli_xz@hotmail.com>
2020-10-19 11:33:21 -0300

    Prueba static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 try:
     from .local_settings import *
 except ImportError:
