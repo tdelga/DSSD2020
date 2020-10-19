b12c7f11fa9e3d3194fae35f55ed2aaceededaec
juliGer <juli_xz@hotmail.com>
2020-10-19 11:28:56 -0300

    Static files storage

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,7 +127,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
