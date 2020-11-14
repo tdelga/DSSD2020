c89e09c20ce8f439023dbf5ffdb56ca64b49af33
Tomas Delgadino <tdelga>
2020-11-14 16:01:41 -0300

    empezamos con el front

---
+++ front/urls.py
@@ -0,0 +1,3 @@
+from django.urls import include, path
+from front import views
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -5,5 +5,7 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
+    path('', include('front.urls')),
+
 
 ]
