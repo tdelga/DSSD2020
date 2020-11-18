d0967ee8483eb65839693897bf65e865df665ff3
juliGer <juli_xz@hotmail.com>
2020-11-18 18:11:53 -0300

    Comienzo local

--- frontend/front/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from django.contrib import admin
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('frontend.urls')),
-
-]
---
+++ local/localapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LocalappConfig(AppConfig):
+    name = 'localapp'
